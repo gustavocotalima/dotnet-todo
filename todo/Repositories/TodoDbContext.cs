03ace7936d75126e804943cc651883cdd960d2cb
Gustavo Lima <gustavocotalima@hotmail.com>
2024-04-20 11:01:31 -0300

    Db context

---
+++ todo/Repositories/TodoDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace todo.Repositories;
+
+public class TodoDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Task> Tasks { get; set; }
+}
